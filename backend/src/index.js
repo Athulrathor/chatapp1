dc86a4c1a41dcda6d742d753e776857b3180a308
Athulrathor <athulrealme2@gmail.com>
2026-03-21 12:48:25 +0530

    testing

--- backend/src/index.js
+++ backend/src/index.js
@@ -34,13 +34,13 @@ app.use(
 app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoutes);
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist"))); // ✅ Fixed typo: fronthend → frontend
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "../frontend/dist"))); // ✅ Fixed typo: fronthend → frontend
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html")); // ✅ Fixed typo: fronthend → frontend
-  });
-}
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html")); // ✅ Fixed typo: fronthend → frontend
+//   });
+// }
 
 connectDB().then(() => {
   server.listen(port, "0.0.0.0", (err) => {
