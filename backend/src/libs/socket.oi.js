2e18929fdd4fac5108e80d61692e0104777977b0
Athulrathor <athulrealme2@gmail.com>
2026-03-21 11:20:53 +0530

    client side and server side connections

--- backend/src/libs/socket.oi.js
+++ backend/src/libs/socket.oi.js
@@ -7,7 +7,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
     cors: {
-        origin: '*',
+        origin: process.env.CLIENT_URL,
         credentials: true,
         methods: ["GET", "POST"],
     },
